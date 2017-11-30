a76a793bcdfc4797bf10f1fa5898572d031de7e8
SimoneLucia <luciasimone1994@gmail.com>
2017-11-30 12:06:38 +0100

    fix

--- botana.py
+++ botana.py
@@ -551,22 +551,21 @@ class BotAna(QtCore.QThread):
         self.send_message(username+" ama "+random.choice(ret_list)+" al "+str(rand)+"% "+emote)
 
     def get_stats(self, user, platform):
-        if platform != "pc" or platform != "xbox" or platform != "ps4":
+        print("user: " + user + " - platform: " + platform + "-")
+        if platform != "pc" and platform != "xbox" and platform != "ps4":
             self.send_message("Errore. Usa !wins <utente> <piattaforma> SeemsGood")
         else:
             URL = "https://fortnitetracker.com/profile/"+platform+"/"+user
+            if '%20' in user:
+                user = user.replace('%20', ' ')
             try:
                 resp = requests.get(URL)
                 player_data = json.loads(self.find(resp.text, 'var playerData = ', ';</script>'))
                 p2 = player_data['p2'][0]['value'] if "p2" in player_data else "N/A"
                 p10 = player_data['p10'][0]['value'] if "p10" in player_data else "N/A"
                 p9 = player_data['p9'][0]['value'] if "p9" in player_data else "N/A"
-                if '%20' in user:
-                    user = user.replace('%20', '')
                 self.send_message("["+user+"] Solo: "+p2+", Duo: "+p10+", Squad: "+p9+" KappaPride ")
             except ValueError:
-                if '%20' in user:
-                    user = user.replace('%20', '')
                 if platform == "ps4" or platform == "xbox":
                     self.send_message("Utente <"+user+"> non trovato BibleThump Assicurati di aver collegato il tuo account PS4 Xbox a quello di EpicGames!")
                 else:
@@ -630,11 +629,13 @@ class BotAna(QtCore.QThread):
 
         elif self.message == "!wins" and not self.is_in_timeout("!wins") and self.is_for_current_game(self.commandsPleb["!wins"]):
             if self.arguments:
+                print("uno")
                 #Separo la stringa per spazi
                 args = self.arguments.split(' ')
                 #Ricompatto la stringa sostituendo agli spazi il %20, l'ultimo argomento è la piattaforma
                 threading.Thread(target=self.get_stats, args=('%20'.join(args[:-1]), args[-1],)).start()
             else:
+                print("due")
                 threading.Thread(target=self.get_stats, args=(["lidfrid", "pc"])).start()
 
         elif self.message == "!patch" and not self.is_in_timeout("!patch") and self.is_for_current_game(self.commandsPleb["!wins"]):
