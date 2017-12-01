7da9a2e74d307d0431ae2ca9d48b84d3ce438bd7
Pinasu <pietro.chico1@gmail.com>
2017-12-01 20:36:52 +0100

    Modified cooldown

--- botana.py
+++ botana.py
@@ -620,7 +620,7 @@ class BotAna(QtCore.QThread):
         rand = randint(0, len(self.quotes)-1)
         q = self.quotes[rand]
         self.send_message("#"+q.get_index()+": ''"+q.get_quote()+" '' - "+q.get_author()+" "+q.get_date())
-        if time.time() - self.text_to_speech > 300:
+        if time.time() - self.text_to_speech > 20:
             threading.Thread(target=self.speak_text, args=(q.get_quote(),)).start()
 
     def get_quote(self, args):
@@ -629,8 +629,8 @@ class BotAna(QtCore.QThread):
         else:
             q = self.quotes[int(args)-1]
             self.send_message("#"+str(q.get_index())+": ''"+q.get_quote()+" '' - "+q.get_author()+" "+q.get_date())
-            if time.time() - self.text_to_speech > 300:
-                threading.Thread(target=self.speak_text, args=(q.get_quote(),)).start()
+            if time.time() - self.text_to_speech > 20:
+                threading.Thread(target=self.speak_text, args=(q.get_author()+" una volta disse: "+q.get_quote(),)).start()
 
     def add_quote(self, args):
         args = str(args)
