46142df146aa30ec7296e61f38a3fc1e9707b3f0
Pinasu <pietro.chico1@gmail.com>
2018-02-28 10:06:32 +0100

    fixes

--- botana.py
+++ botana.py
@@ -80,8 +80,6 @@ class BotAna(QtCore.QThread):
 
         self.timeSkip = 0
 
-        self.greeted = []
-
         self.words_cooldown = dict()
 
         self.msg_count = 0
@@ -225,10 +223,6 @@ class BotAna(QtCore.QThread):
 
                             self.print_message(self.username+": "+self.message)
 
-                            if self.username not in self.greeted and self.username not in self.mods:
-                                self.greeted.append(self.username)
-                                self.send_message("Ciao, "+self.ana(self.username)+"! KappaPride")
-
                             if self.message.startswith('!'):
                                 message_list = self.message.split(' ');
 
@@ -308,15 +302,11 @@ class BotAna(QtCore.QThread):
                     }
                     resp = requests.get(url=url, headers=params)
                     jsonl = json.loads(resp.text)
-                    if jsonl['stream'] == None:
-                      count = 1
-                      self.send_message(x+" ci ha buttato in faccia "+str(count)+" viewer PogChamp Grazie mille Kreygasm Mettete un like su https://www.twitch.tv/"+x+"/ PogChamp")
-
-                    else:
+                    if jsonl['stream'] != None:
                       count = int(jsonl['stream']['viewers'])
-                      self.send_message(x+" ci ha buttato in faccia "+str(count)+" viewers PogChamp Grazie mille Kreygasm Mettete un like su https://www.twitch.tv/"+x+"/ PogChamp")
+                      self.send_message("["+x+"] https://www.twitch.tv/"+x)
 
-                    old = new
+                  old = new
             time.sleep(10)
 
     def get_host_list(self):
