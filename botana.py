758e6adad695cc2007ebdc0960110f1c552bb0ad
Pinasu <pietro.chico1@gmail.com>
2018-04-10 08:28:56 +0200

    Removed useless print

--- botana.py
+++ botana.py
@@ -160,8 +160,6 @@ class BotAna(QtCore.QThread):
         try:
             need_pull = subprocess.Popen(["git", "fetch", "--dry-run"], stdout=subprocess.PIPE, shell=True)
             out, err = need_pull.communicate()
-            print(out)
-            print(err)
 
             if out != 'b''' and err != None:
                 self.restart()
